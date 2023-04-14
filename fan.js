ba21872d722e6a7e3d4b47e1a82b396842a8b107
mary fuller <marymefuller@gmail.com>
2023-04-14 14:41:24 -0400

    first commit

---
+++ fan.js
@@ -0,0 +1,21 @@
+    const Customer = [ {"customer": .378,
+    "name": "Margaret Schmidle",
+    "address":'100', 'Summit', 'Avenue', 'Orchard Park,', 'NY', '14220',
+    "phone": '(716)-674-7466',
+    "status": "Preferred";
+},
+
+    
+    "Customer": .299,
+    "name": "Shannon Poos",
+    "address" '200', 'Good', 'Mews', 'Ct.,', 'Sandy Springs', 'Ga', '30358',
+    'phone' (770)-828-2243,
+    "status": "New";
+}
+
+    {"Customer": .357,
+    "name": "Fred Lehman",
+    'phone' (770)-827-6243,
+    "address" '101' 'Karalyle' 'Ave.' 'Buffalo,' 'NY' '14224',
+    "status": "Elite"; 
+}]
---
+++ store.js
@@ -0,0 +1,21 @@
+const customer =
+[{
+    "id": 17.1,
+    "product": Jersey,
+    "name": "Josh Allen",
+    "photoUrls": ['https://fanatics.frgimages.com/buffalo-bills/mens-nike-josh-allen-royal-buffalo-bills-vapor-untouchable-limited-jersey_pi3161000_ff_3161815_full.jpg?_hv=2&w=340'],
+    "status": "Out of Stock"},
+   
+   
+    {"id": 23.1,
+    "product": Jersey,
+    "name": "Micah Hyde",
+    "photoUrls": ['https://fanatics.frgimages.com/lf?set=key[name],value[HYDE]&set=key[number],value[23]&call=url[http://dmimages.ff.p10/chains/3887265.txt]&scale=size[900]&sink'],
+    "status": "In Stock"},
+
+  { "id": 21.1,
+    "product": Jersey,
+    "name": "Jordan Poyer",
+    "photoUrls": ['https://fanatics.frgimages.com/buffalo-bills/mens-nike-jordan-poyer-royal-buffalo-bills-team-game-jersey_pi4685000_ff_4685748-99af6c417b4852bfc048_full.jpg?_hv=2&w=340'],
+    "status": "In Stock"}]
+  
