31932dc6bbd692bda00458e6121b5511115fca22
mary fuller <marymefuller@gmail.com>
2023-04-15 12:46:36 -0400

    added code

--- fan.js
+++ fan.js
@@ -1,21 +1,21 @@
     const Customer = [ {"customer": .378,
-    "name": "Margaret Schmidle",
-    "address":'100', 'Summit', 'Avenue', 'Orchard Park,', 'NY', '14220',
+    "name": 'Margaret Schmidle',
+    "address":'100 Summit Avenue Orchard Park, NY 14220', 
     "phone": '(716)-674-7466',
-    "status": "Preferred";
+    "status": 'Preferred',
 },
 
     
-    "Customer": .299,
+    {"Customer": .299,
     "name": "Shannon Poos",
-    "address" '200', 'Good', 'Mews', 'Ct.,', 'Sandy Springs', 'Ga', '30358',
-    'phone' (770)-828-2243,
-    "status": "New";
-}
+    "address": '200 Good Mews Ct. Sandy Springs, GA 30358',
+    'phone': '(770)-828-2243',
+    "status": 'New',
+},
 
-    {"Customer": .357,
+{   "Customer": .357,
     "name": "Fred Lehman",
-    'phone' (770)-827-6243,
-    "address" '101' 'Karalyle' 'Ave.' 'Buffalo,' 'NY' '14224',
-    "status": "Elite"; 
+    'phone': (770)-827-6243,
+    "address": '101 Karalyle Ave. Buffalo, NY 14224',
+    "status": "Elite",
 }]
