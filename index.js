945fd6ec6b2f58b116eb0e2b5f994093a436861a
mary fuller <marymefuller@gmail.com>
2023-04-16 19:39:29 -0400

    final7add

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 // importing built in Node modules (HTTP & URL)
-const http = require('http');
-const url = require('url');
+// const http = require('http');
+// const url = require('url');
+
+var express = require('express');
+var server = express()
 
 // assigning post and host name values
 const port = 3000;
 hostname = 'localhost'; 
 
+server.use(require("./storeindex"))
+
 // create the server
-const server = http.createServer((req, res,) => {
-    const urlobj = url.parse(req.url, true);
-    const jersey = urlobj.jersey;
-    console.log (jersey);
-});
+// const server = http.createServer((req, res,) => {
+//     const urlobj = url.parse(req.url, true);
+//     const jersey = urlobj.jersey;
+//     console.log (jersey);
+// });
 
 // make the server listen to request at a given port
-    server.listen(3000, localhost, () => {
-    console.log('the server is running at', '${localhost}:${3000}')
+    server.listen(port, hostname, () => {
+    console.log('the server is running at', `${hostname}:${port}`)
 })
